e666f2cc6a63055b3d0fc385c8b3963c2da780e4
Richard Barr <rbarrera87@gmail.com>
2016-02-09 23:29:57 -0800

    IIFE added and new static function

--- models/todo.js
+++ models/todo.js
@@ -1,9 +1,23 @@
 var mongoose = require('mongoose');
 //Esto va en el archivo que inicializa el proyecto
 //mongoose.connect('mongodb://localhost/todoback');
+(function (){
+  var todoSchema = new mongoose.Schema({
+    name: String
+  });
+  todoSchema.statics.findOrCreate = function(todoObj, done){
+    //Check if the todo item was created before
+    this.findOne({ name: todoObj.name }, function (err, todo) {
+      if(err) return done(err);
 
-var todoSchema = new mongoose.Schema({
-  name: String
-});
+      if(todo) return done(null, todo);
 
-mongoose.model('Todo', todoSchema);
+      todo = new Todo({name: todoObj.name});
+      todo.save(done);
+    })
+  };
+
+  //New variable is created since I am using it within a statistic method
+  //on this script only on findOrCreate
+  var Todo = mongoose.model('Todo', todoSchema);
+})();
