a09f9679b48f1dc6107b4db6d71bde30bdc7bc4a
Richard Barr <rbarrera87@gmail.com>
2016-02-09 23:29:13 -0800

    post to /todos and body-parser added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/todoback');
@@ -8,10 +12,27 @@ require('./models/todo')
 
 var Todo = mongoose.model('Todo');
 
-app.get('/', function(req, res){
+app.get('/todos', function (req, res){
   Todo.find().lean().exec(function (err, todos){
     res.send( JSON.stringify(todos) );
   });
+});
+
+app.post('/todos', function (req, res){
+  console.log("entra");
+  if (req.body.name) {
+    console.log("if");
+    Todo.findOrCreate({
+      name: req.body.name
+    }, function (err, docs){
+      console.log("callback");
+      if (err) {
+        console.log("error");
+        res.send(req.body);
+      }
+      res.send(req.body)
+    });
+  }
 });
 
 app.listen(3000, function(){
